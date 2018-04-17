a48d9e6460c3babc253cfb8d596ccc1c283b9466
lorenzo.riches <lorenzo.riches@wfp.org>
2018-04-17 09:48:15 +0200

    batch norm

--- kerasgen_train.py
+++ kerasgen_train.py
@@ -72,19 +72,25 @@ validation_generator = test_datagen.flow_from_directory(
         target_size=(img_size, img_size),
         batch_size=batch_size)
 # model --------------------------------------------
-from tensorflow.python.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Activation, Dense, Flatten
+from tensorflow.python.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Activation, Dense, Flatten, BatchNormalization
 from tensorflow.python.keras.models import Model
 
 def netowrk(size):
     inputs = Input((size, size, 3))
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1')(inputs)
+    conv1 = BatchNormalization()(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+    pool1 = Dropout(0.25)(pool1)
 
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv2')(pool1)
+    conv2 = BatchNormalization()(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+    pool2 = Dropout(0.25)(pool2)
 
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv3')(pool2)
+    conv3 = BatchNormalization()(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv3)
+    pool3 = Dropout(0.25)(pool3)
 
     dense1 = Dense(256)(pool3)
     dense1 = Activation('relu')(dense1)
